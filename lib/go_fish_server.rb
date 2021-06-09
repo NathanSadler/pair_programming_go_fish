b16238b8ba2b8fd8db6663d475f414fb7440fd00
Nathan Sadler <nssadler@my.waketech.edu>
2021-06-09 16:31:45 -0400

    add accept_client to GoFishServer

--- lib/go_fish_server.rb
+++ lib/go_fish_server.rb
@@ -1,6 +1,26 @@
+require_relative 'player'
 require 'socket'
+
 class GoFishServer
+  attr_reader :server, :players, :clients
   def initialize(port=3336)
-    @
+    @server = TCPServer.new(port)
+    @clients = []
+    @players = []
+  end
+
+  # TODO: add create player part
+  def accept_client
+    clients.push(server.accept_nonblock)
+  rescue IO::WaitReadable
+    ""
+  end
+
+  def create_player_from_client(client)
+    players.push(Player.new(client))
+  end
+
+  def stop
+    server.close
   end
 end
--- spec/go_fish_server_spec.rb
+++ spec/go_fish_server_spec.rb
@@ -1,4 +1,21 @@
 require_relative '../lib/go_fish_server'
+require_relative '../lib/player'
+require 'socket'
+
 describe 'GoFishServer' do
+  let(:server) {GoFishServer.new}
+
+  after(:each) do
+    server.stop
+  end
+
+  context('#accept_client') do
+    it("creates a Player object from a client") do
+      server.accept_client
+      expect(server.clients.empty?).to(eq(true))
+      test_socket = TCPSocket.new('localhost', 3336)
+      expect(server.clients.length).to(eq(0))
+    end
+  end
 
 end
