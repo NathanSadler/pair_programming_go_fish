e12777671ca9c71a1dce11f8d0fa86b066640ef0
Nathan Sadler <nssadler@my.waketech.edu>
2021-06-09 13:45:39 -0400

    Add method/test for returning an array that contains the ranks of each book

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,9 @@
 class Player
-  attr_reader :hand
+  attr_reader :score
+  attr_accessor :hand
   def initialize
     @hand = []
+    @score = 0
   end
 
   def add_card_to_hand(card)
@@ -12,6 +14,25 @@ class Player
     end
   end
 
-  def has_card_with_rank(rank)
+  def has_card_with_rank?(rank)
+    (hand.select {|card| card.rank == rank}).length > 0
+  end
+
+  def remove_cards_with_rank(rank)
+    cards_to_remove = hand.select {|card| card.rank == rank}
+    self.hand -= cards_to_remove
+    cards_to_remove
+  end
+
+  def find_book_ranks
+    occurences = {}
+    hand.each do |card|
+      occurences[card.rank] ? occurences[card.rank] += 1 : occurences[card.rank] = 1
+    end
+    occurences.keys.select {|rank| occurences[rank] == 4}
+  end
+
+  def lay_down_book
+
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,14 @@
 require_relative '../lib/player'
+require 'pry'
 describe 'Player' do
   let(:player) {Player.new}
-  context('.initialize') do
+  context('#initialize') do
     it("doesn't have any cards when created") do
       expect(player.hand).to(eq([]))
     end
   end
-  context('.add_card_to_hand') do
+
+  context('#add_card_to_hand') do
     it("adds a card to the player's hand") do
       test_card = Card.new(rank:"7", suit:"H")
       player.add_card_to_hand(test_card)
@@ -24,4 +26,45 @@ describe 'Player' do
       expect(player.hand).to(eq([Card.new(rank: "9", suit: "D"), test_card]))
     end
   end
+
+  context('#has_card_with_rank?') do
+    before(:each) do
+      player.add_card_to_hand(Card.new(rank: "7", suit: "K"))
+    end
+    it("is true if the player has a card with the specified rank") do
+      expect(player.has_card_with_rank?("7")).to(eq(true))
+    end
+    it("is false if the player doesn't have a card with the specified rank") do
+      expect(player.has_card_with_rank?("8")).to(eq(false))
+    end
+  end
+
+  context('#remove_cards_with_rank') do
+    it('returns and removes all cards with a specified rank') do
+      cards = [Card.new(rank: "8", suit: "S"), Card.new(rank: "8", suit:"D"),
+        Card.new(rank: "4", suit: "H")]
+      player.add_card_to_hand(cards)
+      removed_cards = player.remove_cards_with_rank("8")
+      expect(player.hand).to(eq([Card.new(rank: "4", suit: "H")]))
+      expect(removed_cards).to(eq([Card.new(rank: "8", suit: "S"), Card.new(rank: "8", suit:"D")]))
+    end
+  end
+
+  context('#find_book_ranks') do
+    it("returns an array the ranks of each book") do
+      book = ["S", "D", "H", "C"].map {|suit| Card.new(rank: "7", suit: suit)}
+      player.add_card_to_hand(book)
+      player.add_card_to_hand(Card.new(rank: "4", suit: "S"))
+      expect(player.find_book_ranks).to(eq(["7"]))
+    end
+  end
+
+  # context('#lay_down_book') do
+  #   it("removes a book from the hand and increases the player's score") do
+  #     book = ["S", "D", "H", "C"].map {|suit| Card.new(rank: "7", suit: suit)}
+  #     player.add_card_to_hand(book)
+  #     player.lay_down_book
+  #     expect(player.score).to(eq(1))
+  #   end
+  # end
 end
