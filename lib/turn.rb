bb39356d4faafda916537da4f061fa90d79f2efa
Nathan Sadler <nssadler@my.waketech.edu>
2021-06-11 10:26:20 -0400

    add list_other_players method to turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,4 +4,14 @@ class Turn
     @player = player
     @game = game
   end
+
+  def list_other_players
+    other_players = game.players.select {|other_player| other_player != player}
+    counter, message = [1, ""]
+    other_players.each  do |listable_player|
+      message += "#{counter}: #{listable_player.name}" + "\n"
+      counter += 1
+    end
+    game.send_message_to_player(game.players.index(player), message)
+  end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/turn'
 require_relative '../lib/go_fish_server'
+require_relative '../lib/go_fish_client'
 describe 'Turn' do
   context('#initialize') do
     it("creates a new Turn object using a Player and Game") do
@@ -16,7 +17,10 @@ describe 'Turn' do
     let(:client_list) {[]}
     before(:each) do
       ["Huey", "Duey", "Lebron"].each do |name|
-        
+        server.accept_client
+        client_list.push(GoFishClient.new)
+        server.try_to_add_player_to_game
+        server.waiting_game.players[-1].set_user_name(name)
       end
     end
     after(:each) do
@@ -24,13 +28,37 @@ describe 'Turn' do
       server.stop
     end
     it("returns the player corresponding to the number the user enters") do
-
-    end
-    it("doesn't list the user being prompted") do
-
+      client_list[0].provide_input("1")
+      turn = Turn.new(server.waiting_game.players[0], server.waiting_game)
+      selected_player = turn.select_other_player
     end
     it("retries if the user doesn't give a valid response") do
 
+    end
+  end
+
+  context('#list_other_players') do
+    let!(:server) {GoFishServer.new}
+    let(:game) {Game.new}
+    let(:client_list) {[]}
+    before(:each) do
+      ["Huey", "Duey", "Lebron"].each do |name|
+        server.accept_client
+        client_list.push(GoFishClient.new)
+        server.try_to_add_player_to_game
+        server.waiting_game.players[-1].set_user_name(name)
+      end
+    end
+    after(:each) do
+      client_list.each {|client| client.close}
+      server.stop
+    end
+    it("returns a list of all players except for the user seeing the list") do
+      turn = Turn.new(server.waiting_game.players[1], server.waiting_game)
+      turn.list_other_players
+      user_output = client_list[1].capture_output
+      expect(user_output.include?("1: Huey")).to(eq(true))
+      expect(user_output.include?("2: Lebron")).to(eq(true))
     end
   end
 end
