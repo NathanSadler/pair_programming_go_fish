9d62644d98c202bfdd728504f6f993a9f4bab2ec
Nathan Sadler <nssadler@my.waketech.edu>
2021-06-11 09:22:19 -0400

    add game argument to Turn's initialize method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,7 @@
 class Turn
-  attr_accessor :player
-  def initialize(player)
+  attr_accessor :player, :game
+  def initialize(player, game)
     @player = player
+    @game = game
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/turn'
 describe 'Turn' do
   context('#initialize') do
-    it("creates a new Turn object using a Player") do
+    it("creates a new Turn object using a Player and Game") do
       test_player = Player.new
-      test_turn = Turn.new(test_player)
+      test_game = Game.new
+      test_turn = Turn.new(test_player, test_game)
       expect(test_turn.player).to(eq(test_player))
+      expect(test_turn.game).to(eq(test_game))
     end
   end
 end
